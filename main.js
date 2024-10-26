4d219b0f91a6e51f85e558ea9984e97d37bbf84c
Najaf <najafshaikh1705@gmail.com>
2024-10-26 20:04:00 +0530

    zoro

--- main.js
+++ main.js
@@ -8,15 +8,19 @@ const render=()=>{
     for(let i=0;i<3;i++){
         for(let j=0;j<3;j++){
             if(mat[i][j]=='x'){
-                document.getElementById(elms[i][j]).innerHTML="<i class='fa-solid fa-x'></i>";
+                document.getElementById(elms[i][j]).innerHTML='<i class="fa-solid fa-x" style="color: #ffffff;"></i>';
             }
             else if(mat[i][j]=='o'){
-                document.getElementById(elms[i][j]).innerHTML="<i class='fa-solid fa-o'></i>";
+                document.getElementById(elms[i][j]).innerHTML='<i class="fa-solid fa-o" style="color: #ffffff;"></i>';
             }
             else{
                 document.getElementById(elms[i][j]).innerHTML="";
             }
         }
+    }
+    if(events.length===6){
+        const [x,y]=events[0];
+        document.getElementById(""+x+y).style.backgroundColor="#629bff";
     }
 }
 
@@ -33,6 +37,7 @@ const clicked=(element)=>{
         if(events.length>6){
             const [oldX, oldY] = events[0];
             mat[oldX][oldY]='0'
+            document.getElementById(""+oldX+oldY).style.backgroundColor="black";
             events.splice(0,1);
         }
         if(isX===true){
@@ -55,63 +60,66 @@ const clicked=(element)=>{
 const checkWinner=()=>{
     for(let i=0;i<3;i++){
         if(mat[i][0]==='x' && mat[i][1]==='x' && mat[i][2]==='x'){
-            console.log(i+"1")
-            document.getElementById(i+"0").style.backgroundColor="red";
-            document.getElementById(i+"1").style.backgroundColor="red";
-            document.getElementById(i+"2").style.backgroundColor="red";
+            for(let j of ["0","1","2"]){
+                document.getElementById(i+j).style.backgroundColor="green";
+            }
             document.getElementById("winner").innerHTML="X won"
             winnerFound=true;
         }
         if(mat[i][0]==='o' && mat[i][1]==='o' && mat[i][2]==='o'){
-            document.getElementById(i+"0").style.backgroundColor="red";
-            document.getElementById(i+"1").style.backgroundColor="red";
-            document.getElementById(i+"2").style.backgroundColor="red";
+            for(let j of ["0","1","2"]){
+                document.getElementById(i+j).style.backgroundColor="green";
+            }
             document.getElementById("winner").innerHTML="O won"
             winnerFound=true;
         }
     }
+
     for(let i=0;i<3;i++){
         if(mat[0][i]==='x' && mat[1][i]==='x' && mat[2][i]==='x'){
-            document.getElementById("0"+i).style.backgroundColor="red";
-            document.getElementById("1"+i).style.backgroundColor="red";
-            document.getElementById("2"+i).style.backgroundColor="red";
+            for(let j of ["0","1","2"]){
+                document.getElementById(j+i).style.backgroundColor="green";
+            }
             document.getElementById("winner").innerHTML="X won"
             winnerFound=true;
         }
         if(mat[0][i]==='o' && mat[1][i]==='o' && mat[2][i]==='o'){
-            document.getElementById("0"+i).style.backgroundColor="red";
-            document.getElementById("1"+i).style.backgroundColor="red";
-            document.getElementById("2"+i).style.backgroundColor="red";
+            for(let j of ["0","1","2"]){
+                document.getElementById(j+i).style.backgroundColor="green";
+            }
             document.getElementById("winner").innerHTML="O won"
             winnerFound=true;
         }
     }
+
     if(mat[0][0]==='x' && mat[1][1]==='x' && mat[2][2]==='x'){
-        document.getElementById("00").style.backgroundColor="red";
-        document.getElementById("11").style.backgroundColor="red";
-        document.getElementById("22").style.backgroundColor="red";
+        for(let i of ["00","11","22"]){
+            document.getElementById(i).style.backgroundColor="green";
+        }
         document.getElementById("winner").innerHTML="X won"
         winnerFound=true;
     }
+
     if(mat[0][2]==='x' && mat[1][1]==='x' && mat[2][0]==='x'){
-        document.getElementById("02").style.backgroundColor="red";
-        document.getElementById("11").style.backgroundColor="red";
-        document.getElementById("20").style.backgroundColor="red";
+        for(let i of ["02","11","20"]){
+            document.getElementById(i).style.backgroundColor="green";
+        }
         document.getElementById("winner").innerHTML="X won"
         winnerFound=true;
     }
 
     if(mat[0][0]==='o' && mat[1][1]==='o' && mat[2][2]==='o'){
-        document.getElementById("00").style.backgroundColor="red";
-        document.getElementById("11").style.backgroundColor="red";
-        document.getElementById("22").style.backgroundColor="red";
+        for(let i of ["00","11","22"]){
+            document.getElementById(i).style.backgroundColor="green";
+        }
         document.getElementById("winner").innerHTML="O won"
         winnerFound=true;
     }
+
     if(mat[0][2]==='o' && mat[1][1]==='o' && mat[2][0]==='o'){
-        document.getElementById("02").style.backgroundColor="red";
-        document.getElementById("11").style.backgroundColor="red";
-        document.getElementById("20").style.backgroundColor="red";
+        for(let i of ["02","11","20"]){
+            document.getElementById(i).style.backgroundColor="green";
+        }
         document.getElementById("winner").innerHTML="O won"
         winnerFound=true;
     }
@@ -129,7 +137,7 @@ const restart=()=>{
     winnerFound=false;
     for(let i=0;i<3;i++){
         for(let j=0;j<3;j++){
-            document.getElementById(""+i+j).style.backgroundColor="#CADCFC";
+            document.getElementById(""+i+j).style.backgroundColor="black";
         }
     }
     document.getElementById("winner").innerHTML="";
